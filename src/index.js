504eb00ea1e8535a2a65a2cbc2dd18065a5eb0af
Daniel Garcia <dlgarciabr@gmail.com>
2021-04-08 15:58:24 +0100

    adding CORS settings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const url = require('url');
 const axios = require('axios');
 
 const requestListener =  (req, res) => {
-    
+      
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+  
     const currentUserId = url.parse(req.url).query.split("=")[1];
     const userStatusUrl = `https://api.roblox.com/users/${currentUserId}/onlinestatus/`;
 
