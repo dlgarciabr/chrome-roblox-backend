ba58e432416869decc9bbb376fb7a0db684b1967
Daniel Garcia <dlgarciabr@gmail.com>
2021-04-08 15:39:16 +0100

    initial version

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+const axios = require('axios');
+
+const requestListener =  (req, res) => {
+    
+    const currentUserId = url.parse(req.url).query.split("=")[1];
+    const userStatusUrl = `https://api.roblox.com/users/${currentUserId}/onlinestatus/`;
+
+      axios.get(userStatusUrl)
+      .then(function (response) {
+        // handle success
+        res.writeHead(200);
+        res.end(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+        res.writeHead(200);
+      });
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
